dbfc03754a8975043f28daa488fe6cfef7e8cef1
indogusmas <indogusmasarungsamudra@gmail.com>
2020-06-22 01:05:23 +0700

    add token

--- relationship/authors/views.py
+++ relationship/authors/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from .serializer import UserSerializer,AuthorSerializer,BookSerializer
 from .models import Author, Book
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,6 +17,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    permission_class = (IsAuthenticated)
     queryset = Book.objects.all()
     serializer_class =  BookSerializer
     filter_backends = [filters.OrderingFilter]
--- relationship/relationship/settings.py
+++ relationship/relationship/settings.py
@@ -72,7 +72,15 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.IsAuthenticated',
+    ),
 
+}
 WSGI_APPLICATION = 'relationship.wsgi.application'
 
 
